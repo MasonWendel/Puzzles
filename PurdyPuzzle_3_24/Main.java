641402cfb33969ec5d54d9bb38374c2d28f12b55
Mason Wendel <Masonwendel@outlook.com>
2024-04-29 12:46:55 -0500

    Started something.Didn't finish it

---
+++ PurdyPuzzle_3_24/Main.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+import java.util.*; 
+
+public class Main{
+    public static void main(String[] args) {
+        BigDecimal bigGuy = new BigDecimal(0); 
+
+        bigGuy.setScale(314);
+
+        
+        System.out.println(bigGuy);
+
+    }
+}
